a2bef847fb5aedd46fef332f60275a084503795a
IraquianRodrigues <iraquianrodrigues@hotmail.com>
2025-02-25 15:25:31 -0300

    carrosel

--- src/app/_components/about.tsx
+++ src/app/_components/about.tsx
@@ -1,13 +1,16 @@
 import Image from "next/image"
 import about1Img from "../../../public/about-1.png"
 import about2Img  from "../../../public/about-2.png"
-import { Check } from "lucide-react"
+import { Check, MapPin } from "lucide-react"
 import { WhatsappLogo } from "@phosphor-icons/react/dist/ssr"
+
 
 export function About(){
     return(
-        <section className="bg-[#fdf6ec] py-16 ">
-            <div className="container mx-auto grid cols-1 ld: grid-cols-2 gap-12 items-center">
+        <section className="bg-[#fdf6ec] py-16 mt-10">
+            <div className="container mx-auto">
+
+              <div className="grid cols-1 ld: grid-cols-2 gap-12 items-center px-4">
 
               <div className="relative">
                 <div className="relative w-full h-[400px] rounded-3xl overflow-hidden">
@@ -32,8 +35,7 @@ export function About(){
                 </div>
 
               </div>
-
-            
+         
               <div className="space-y-6">
                 <h2 className="text-4xl font-bold">Sobre</h2>
 
@@ -56,14 +58,25 @@ export function About(){
                     </li>
                 </ul>
 
-                <a href="#"
-                className="bg-green-500 text-white font-semibold flex items-center justify-center w-fit gap-2 px-4 py-2 rounded-sm"
+            <div className="flex gap-2">
+            <a href="#"
+                className="bg-black text-white font-semibold flex items-center justify-center w-fit gap-2 px-4 py-2 rounded-sm"
                 >
                     <WhatsappLogo className="w-5 h-5 text-white"/>
                     Contato via WhatsApp
                 </a>
-              </div>
-          
+               
+                <a href="#"
+                className="font-semibold flex items-center justify-center w-fit gap-2 px-4 py-2 rounded-sm"
+                >
+                    <MapPin className="w-5 h-5 text-black"/>
+                    Endereço da loja
+                </a>
+            </div>
+
+             </div>
+             
+              </div>   
             </div>
         </section>
     )
